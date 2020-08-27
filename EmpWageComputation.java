2b4586b985a8de5913180ae1e4b913c1b48bdfbd
Sagar <sagarrashinkar2@gmail.com>
2020-08-28 00:56:26 +0530

    ADDED: computing wage till condition

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,16 +2,20 @@ class EmpWageComputation {
 
 	public static final int isFullTime = 1;
 	public static final int isPartTime = 2;
-	public static final int wagePerHour = 20; 
+	public static final int wagePerHour = 20;
 	public static final int workDayPerMonth = 20; 
+	public static final int workHourPerMonth = 100; 
 
 	public static void main(String[] args) {
 	
 		int workHour = 0;
 		int totalWorkHour = 0;
 		int dailyWage = 0;
+		int totalWorkDay = 0;
 		
-		for(int i=1;i<=workDayPerMonth;i++){
+		while(totalWorkHour<workHourPerMonth && totalWorkDay < workDayPerMonth){
+			
+			totalWorkDay++;
 			int attendence = (int)(Math.random()*3)+1;
 		
 			switch(attendence){
@@ -28,7 +32,7 @@ class EmpWageComputation {
 			}
 			totalWorkHour += workHour;
 			dailyWage = workHour * wagePerHour;
-			System.out.println("wage of day "+i+" is: "+dailyWage);
+			System.out.println("wage of day "+totalWorkDay+" is: "+dailyWage);
 		}
 		int monthWage = wagePerHour * totalWorkHour;
 		System.out.println("Total wage of month is: "+monthWage);
