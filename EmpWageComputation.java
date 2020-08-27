86629742871a6c68b55d7192f5b3ba545ae2c739
Sagar <sagarrashinkar2@gmail.com>
2020-08-27 20:21:25 +0530

    ADDED: Daily wages added

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,16 +1,26 @@
 class EmpWageComputation {
 
+	public static int isFullDay = 1;
+	public static int wagePerHour = 20; 
+
 	public static void main(String[] args) {
 	
+		int workHour = 0;
+		int totalWorkHour = 0;
+		int dailyWage = 0;
 		int attendence = (int)(Math.random()*2)+1;
 		
-		if(attendence == 1) {
+		if(isFullDay == attendence) {
 		
-			System.out.println("Employee is present");
+			workHour = 8;
 		}
 		else {
 		
-			System.out.println("Employee is absent");
+			workHour = 0;
 		}
+		totalWorkHour += workHour;
+		dailyWage = totalWorkHour * wagePerHour;
+		System.out.println("Daily wage is: "+dailyWage);
+
 	}
 }
